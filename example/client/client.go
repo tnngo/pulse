4aed50219ec3b5409e716923dd452b8f6ad24a2a
tnngo <tnngou@gmail.com>
2021-04-25 04:44:54 +0800

    update: delete the commented out code

--- example/client/client.go
+++ example/client/client.go
@@ -21,40 +21,5 @@ func main() {
 	time.Sleep(1 * time.Second)
 	c.WriteRoute(1, []byte("test"))
 
-	//p := new(packet.Packet)
-	//p.Type = packet.Type_Connack
-	//b1, err := proto.Marshal(p)
-	//if err != nil {
-	//	log.L().Error(err.Error())
-	//	return
-	//}
-	//b2 := proto.EncodeVarint(uint64(len(b1)))
-	//b2 = append(b2, b1...)
-	//conn.Write(b2)
-
-	// b, err := os.ReadFile("./thumb.jpeg")
-	// if err != nil {
-	// 	log.L().Error(err.Error())
-	// 	return
-	// }
-
-	// log.L().Debug("图片大小", zap.Int("b", len(b)))
-
-	// p := new(packet.Packet)
-	// p.Msg = b
-	// b1, err := proto.Marshal(p)
-	// if err != nil {
-	// 	log.L().Error(err.Error())
-	// 	return
-	// }
-
-	// b2 := proto.EncodeVarint(uint64(len(b1)))
-	// b2 = append(b2, b1...)
-	// log.L().Debug("protobuf大小", zap.Int("b2", len(b2)))
-	// for {
-	// 	time.Sleep(10 * time.Millisecond)
-	// 	conn.Write(b2)
-	// }
-
 	select {}
 }
