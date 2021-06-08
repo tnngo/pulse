fbf714f3da90099d619979417ca18d6685537ada
tnngo <tnngou@gmail.com>
2021-06-08 16:15:50 +0800

    update: should not prevent msg=nil

--- client/client.go
+++ client/client.go
@@ -213,11 +213,6 @@ func (c *Client) route(p *packet.Packet) {
 		return
 	}
 
-	if p.Msg == nil {
-		log.L().Warn("packet.Msg is nil")
-		return
-	}
-
 	if p.Route == nil {
 		log.L().Warn("packet.Route is nil")
 		return
